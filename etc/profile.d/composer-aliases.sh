8449a50c64c2f4bde6cb49b72cf5d69e673d5840
tarkhov <agtuks@gmail.com>
2019-06-29 16:04:49 +0300

    commit

--- etc/profile.d/composer-aliases.sh
+++ etc/profile.d/composer-aliases.sh
@@ -3,6 +3,7 @@ alias cgr='composer global require'
 alias ci='composer install'
 alias cr='composer require'
 alias crd='composer require --dev'
+alias crm='composer remove'
 alias cu='composer update'
 
 alias u='./vendor/bin/phpunit'
